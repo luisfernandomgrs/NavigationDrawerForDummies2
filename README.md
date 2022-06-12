6fd05e192cbc9f79fcb8554882600797a0416f37
Luis Fernando Machado <luisfernandomgrs@gmail.com>
2022-06-12 01:59:23 -0300

    first commit, but the exemplo is fully functional

---
+++ README.md
@@ -0,0 +1,1 @@
+Application to learning about use component Navigation Drawer and the component Fragment
---
+++ app/src/main/java/com/luisf/learning/appatmnavigationdrawer/ui/principal/PrincipalFragment.java
@@ -0,0 +1,25 @@
+package com.luisf.learning.appatmnavigationdrawer.ui.principal;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.luisf.learning.appatmnavigationdrawer.R;
+
+public class PrincipalFragment extends Fragment {
+
+    public PrincipalFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_home, container, false);
+    }
+}
