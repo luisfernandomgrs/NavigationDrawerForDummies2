42fd6f4ed2fef88bd2af9697aaf1ce83fd393bec
Luis Fernando Machado <luisfernandomgrs@gmail.com>
2022-06-12 04:34:07 -0300

    Using the component AboutPage

--- app/src/main/java/com/luisf/learning/appatmnavigationdrawer/ui/sobre/SobreFragment.java
+++ app/src/main/java/com/luisf/learning/appatmnavigationdrawer/ui/sobre/SobreFragment.java
@@ -12,6 +12,9 @@ import android.widget.Button;
 import com.google.android.material.snackbar.Snackbar;
 import com.luisf.learning.appatmnavigationdrawer.R;
 
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
 public class SobreFragment extends Fragment {
     private Button buttonActionDial;
 
@@ -24,6 +27,32 @@ public class SobreFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sobre, container, false);
+        //return inflater.inflate(R.layout.fragment_sobre, container, false);
+
+        Element versao = new Element();
+        versao.setTitle("Versão 1.0");
+
+        String descricao = "A ATM Consultoria tem como missão apoiar organizações " +
+                "que desejam alcançar o sucesso através da excelência em gestão e " +
+                "da busca pela qualidade.";
+        return new AboutPage(getActivity())
+                .setImage(R.drawable.logo)
+                .setDescription(descricao)
+
+                .addGroup("Entre em contato")
+                .addEmail("atendimento@atmconsultoria.com.br", "Envie um e-mail")
+                .addWebsite("https://appatmconsultoria.com/", "Acesse nosso site")
+
+                .addGroup("Redes Sociais")
+                .addFacebook("your_id", "Facebook")
+                .addTwitter("your_id", "Twitter")
+                .addYoutube("your_channel_id","Youtube")
+                .addPlayStore("your url at PlayStore","PlayStore")
+                .addGitHub("luisfernandomgrs","GitHub")
+                .addInstagram("your_id","Instagram")
+
+                .addItem(versao)
+
+                .create();
     }
 }
